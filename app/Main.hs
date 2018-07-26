d5d6f9dd79dad1bed33f7bd4c14b2a39788808f8
meck <johan@meck.se>
2018-07-26 15:49:29 +0200

    Color Rendering

--- app/Main.hs
+++ app/Main.hs
@@ -26,7 +26,7 @@ data World = World { evnt :: Env
 
 
 initalWorld = World
-  { evnt     = Env 20 0.5 300
+  { evnt     = Env 20 0.5 50
   , spkrs    = initalSpkrs
   , viewSize = initalWinSize
   , viewOrig = (0, 0)
@@ -38,26 +38,28 @@ makePict w = return $ uncurry makePicture (viewSize w) 1 1 (pointColor w) -- TOD
 pointColor :: World -> Point -> Color -- TODO Clean up add offset
 pointColor (World e sp vs vo) p = dbToCol totDb
  where
-  p'    = (fst p * 10, snd p * 10)
+  p'    = (fst p * 20, snd p * 20)
   totDb = audioVecToSpl $ runReader (totalAtPoint p' sp) e
 
 dbToCol :: Double -> Color
--- dbToCol x = rgb (3.0 * x') (2.0 * (1 - x')) 0 where 
-dbToCol x = rgb x'' (1 - x'') 0
+dbToCol x = rgb' scalR scalG 0
  where
-  rMaxGmin = 120
-  rMinGMax = 50
-  d        = rMaxGmin - rMinGMax
-  x'       = realToFrac $ (x - rMinGMax) / d
-  x''      = x' - 1 / realToFrac d
+  sMax  = 130
+  sMin  = 70
+  sMid  = sMin + (sMax - sMin) / 2
+  scalR = realToFrac $ scal $ (x - sMin) / (sMax - sMin)
+  scalG = realToFrac $ scal $ (x - sMin) / (sMid - sMin)
+  scal x | x < 0     = 0
+         | x > 1     = 1
+         | otherwise = x
 
 initalSpkrs =
-  [ idealSpeaker { pos = (1, 1) }
-  , idealSpeaker { pos = (-1, -1) }
-  , idealSpeaker { pos = (-1, 1) }
-  , idealSpeaker { pos = (1, -1) }
+  [ idealSpeaker { pos = (0.75, -1), dly = 0.040 }
+  , idealSpeaker { pos = (0.25, -1) }
+  , idealSpeaker { pos = (0, -1) }
+  , idealSpeaker { pos = (-0.25, -1) }
+  , idealSpeaker { pos = (-0.75, -1), dly = 0.040 }
   ]
-
 
 main :: IO ()
 main = interactIO (InWindow "sub" initalWinSize initalWinPos)
