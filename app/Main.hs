941e30070cffb716730dcf9412a117a528550556
meck <johan@meck.se>
2018-08-07 00:46:08 +0200

    fixed scaling

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import           Graphics.Gloss
 import           Control.Monad.Reader
 import           Data.Complex
 import           Data.IORef
+import           Data.Bifunctor
 
 winName :: String
 winName = "Speak Plot"
@@ -20,7 +21,8 @@ initalWinPos = (100, 100)
 resol :: Int
 resol = 1
 
-pixPerMeter = 20
+zoomFact :: Float
+zoomFact = 5
 
 iDisplay :: Display
 iDisplay = InWindow winName initalWinSize initalWinPos
@@ -28,38 +30,43 @@ iDisplay = InWindow winName initalWinSize initalWinPos
 data World = World { evnt :: Env
                    , spkrs :: [Speaker]
                    , viewSize :: (Int, Int)
-                   , viewOrig :: (Float, Float) }
+                   , viewOrig :: (Float, Float)
+                   , pixPerM :: Float
+                   }
+
 
 
 iWorld = World
-  { evnt     = Env (Just Atmos {tmp = 20, hum = 0.5, pres = 101.325}) 1000.0
+  { evnt     = Env (Just Atmos {tmp = 20, hum = 0.5, pres = 101.325}) 100.0
   , spkrs    = iSpeak
   , viewSize = initalWinSize
   , viewOrig = (0, 0)
+  , pixPerM  = 20
   }
 
 iSpeak =
-  [idealSpeaker { pos = (0.0, 5.0) }, idealSpeaker { pos = (0.0, -5.0) }]
+  [ idealSpeaker { pos = (0.0, 0.0), dly = 0.0025 }
+  , idealSpeaker { pos = (0.0, -0.8575), polInv = True }
+  ]
 
 makePict :: World -> IO Picture
 makePict w =
   return
     $ pictures
-    $ uncurry makePicture (1000, 1000) resol resol (pointColor w)
-    : (drawSpeaker <$> spkrs w)
+    $ uncurry makePicture (viewSize w) resol resol (pointColor w)
+    : (drawSpeaker (pixPerM w) <$> spkrs w)
 
 pointColor :: World -> Point -> Color
-pointColor (World e sp vs vo) p = dbToCol totDb
+pointColor (World e sp vs vo ppm) p = dbToCol totDb
  where
-  p'    = bimap (pixPerMeter *) p
--- p'    = bimap (/ pixPerMeter) p
+  p'    = uncurry bimap (bimap' ((*) . (/ 2) . (/ ppm) . fromIntegral) vs) p -- TODO /2 for 2x res?
   totDb = audioVecToSpl $ runReader (totalAtPoint p' sp) e
 
 dbToCol :: Double -> Color
 dbToCol x = rgb' scalR scalG 0
  where
   sMax  = 100
-  sMin  = 65
+  sMin  = 60
   sMid  = sMin + (sMax - sMin) / 2
   scalR = realToFrac $ scal $ (x - sMin) / (sMax - sMin)
   scalG = realToFrac $ scal $ (x - sMin) / (sMid - sMin)
@@ -87,6 +94,13 @@ eventHandler c e w = do
     --     c'
     --     (\vp -> return $ vp { viewPortScale = 4.0 })
       -- return w
+    -- EventKey k Down _ _ -> do
+    --   print k
+    --   return w
+    EventKey (Char '-') Down _ _ ->
+      return $ w { pixPerM = pixPerM w - zoomFact }
+    EventKey (Char '=') Down _ _ ->
+      return $ w { pixPerM = pixPerM w + zoomFact }
     EventKey{}    -> return w
     EventMotion{} -> return w
     EventResize s -> do
@@ -94,11 +108,11 @@ eventHandler c e w = do
       return w { viewSize = s }
 
 
-drawSpeaker :: Speaker -> Picture
-drawSpeaker s =
-  color blue
-    $ uncurry translate (bimap (* pixPerMeter) (pos s))
-    $ scale pixPerMeter pixPerMeter
+drawSpeaker :: Float -> Speaker -> Picture
+drawSpeaker sc s =
+  color (greyN 0.7)
+    $ uncurry translate (bimap' (* sc) (pos s))
+    $ scale sc sc
     $ polygon
     $ uncurry rectanglePath
     $ size s
@@ -113,5 +127,5 @@ idealSpeaker = Speaker
   , size   = (1, 1)
   }
 
-bimap :: (t -> b) -> (t, t) -> (b, b)
-bimap f (x, y) = (f x, f y)
+bimap' :: Bifunctor p => (a -> d) -> p a a -> p d d
+bimap' f = bimap f f
